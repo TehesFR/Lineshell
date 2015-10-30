2c339dfd882b85f91e669d7992a9ca0e4f2e105d
Tehes <TehesFR@users.noreply.github.com>
2015-10-30 23:56:47 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://lineshell_user:lineshell_pass@localhost:lineshell_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://lineshell_user:lineshell_pass@localhost:lineshell_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/<db>
 
 };
